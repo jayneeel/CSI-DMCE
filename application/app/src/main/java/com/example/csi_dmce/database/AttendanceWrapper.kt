9e9687b5e512b34472cff53eb9bdc09893d16c22
Bourbon <amitkulkarni2028@gmail.com>
2023-02-26 18:04:09 +0530

    refactor: attendance wrappers

--- application/app/src/main/java/com/example/csi_dmce/database/AttendanceWrapper.kt
+++ application/app/src/main/java/com/example/csi_dmce/database/AttendanceWrapper.kt
@@ -1,15 +1,14 @@
 package com.example.csi_dmce.database
 
-import com.google.firebase.firestore.CollectionReference
 import kotlinx.coroutines.tasks.await
 import com.google.firebase.firestore.DocumentId
 import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 
 data class Attendance(
     @DocumentId
-    var student_id: String?     = null,
-    var event_uuid: String?       = null,
+    var event_uuid: String?     = null,
     var first: Boolean?         = false,
     var second: Boolean?        = false,
 )
@@ -18,44 +17,39 @@ class AttendanceWrapper {
     companion object{
         private val attendanceCollectionRef = FirebaseFirestore.getInstance().collection("attendance")
 
-        private fun getAttendanceDocument(attendanceCollectionRef: CollectionReference, studentID: String, eventUUID: String) : DocumentReference{
+        private suspend fun getAttendanceDocumentReference(studentID: String, eventUUID: String) : DocumentReference {
             return attendanceCollectionRef.document(studentID)
                 .collection("events")
                 .document(eventUUID)
         }
 
-        suspend fun getAttendance(studentID: String, eventUUID: String) : Attendance? {
-            val attendanceDocument = getAttendanceDocument(attendanceCollectionRef, studentID, eventUUID).get().await()
+        private suspend fun getAttendanceDocument(studentID: String, eventUUID: String) : DocumentSnapshot {
+            return attendanceCollectionRef.document(studentID)
+                .collection("events")
+                .document(eventUUID)
+                .get()
+                .await()
+        }
+
+        private suspend fun getAttendanceObject(studentID: String, eventUUID: String) : Attendance? {
+            val attendanceDocument = getAttendanceDocument(studentID, eventUUID)
             return attendanceDocument.toObject(Attendance::class.java)
         }
 
-        suspend fun setFirstQRAttendance(studentID: String, eventUUID: String, scanningStatus1: Boolean){
-            attendanceCollectionRef
-                .document(studentID)
-                .collection("events")
-                .document(eventUUID)
-                .update("first", scanningStatus1)
+        suspend fun setQRAttendance(studentID: String, eventUUID: String, attendanceType: String, attendanceStatus: Boolean){
+            val attendanceDocumentReference = getAttendanceDocumentReference(studentID, eventUUID)
+            attendanceDocumentReference
+                .update(attendanceType, attendanceStatus)
                 .await()
         }
 
-        suspend fun setSecondQRAttendance(studentID: String, eventUUID: String, scanningStatus2: Boolean){
-            attendanceCollectionRef
-                .document(studentID)
-                .collection("events")
-                .document(eventUUID)
-                .update("second", scanningStatus2)
-                .await()
+        suspend fun checkAttendanceStatus(studentID: String, eventUUID: String, attendanceType: String): Boolean? {
+            val attendanceDocument = getAttendanceObject(studentID, eventUUID)
+            return when(attendanceType) {
+                "first" -> attendanceDocument?.first
+                "second" -> attendanceDocument?.second
+                else -> throw NotImplementedError("Invalid attendance type.")
+            }
         }
-
-        suspend fun checkAttendanceStatus1(studentID: String, eventUUID: String): Boolean? {
-            val attendanceDocument = getAttendance(studentID, eventUUID)
-            return attendanceDocument?.first
-        }
-
-        suspend fun checkAttendanceStatus2(studentID: String, eventUUID: String): Boolean? {
-            val attendanceDocument = getAttendance(studentID, eventUUID)
-            return attendanceDocument?.second
-        }
-
     }
 }
