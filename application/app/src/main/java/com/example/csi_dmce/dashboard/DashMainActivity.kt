16adf5bf2a8e940972efa0c31dee8264fea6f140
Bourbon <amitkulkarni2028@gmail.com>
2023-04-17 04:24:26 +0530

    make nav header stuff work

--- application/app/src/main/java/com/example/csi_dmce/dashboard/DashMainActivity.kt
+++ application/app/src/main/java/com/example/csi_dmce/dashboard/DashMainActivity.kt
@@ -4,31 +4,61 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.csi_admin.complaint.ComplaintLodge
 import com.example.csi_dmce.R
 import com.example.csi_dmce.auth.CsiAuthWrapper
+import com.example.csi_dmce.database.Student
+import com.example.csi_dmce.database.StudentWrapper
 import com.example.csi_dmce.ui.WelcomeActivity
 import com.example.csiappdashboard.ProfileFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
+import kotlinx.coroutines.runBlocking
 
 
 class DashMainActivity : AppCompatActivity() {
     lateinit var bottomNavBar : BottomNavigationView
     lateinit var toggle: ActionBarDrawerToggle
+    lateinit var studentObject: Student
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbars))
 
+        studentObject = runBlocking {
+            StudentWrapper.getStudent(CsiAuthWrapper.getStudentId(applicationContext))!!
+        }
+
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView : NavigationView = findViewById(R.id.nav_view)
         navView.itemIconTintList = null
+
+        val navHeaderView = navView.getHeaderView(0)
+
+        val navHeaderImage: ImageView = navHeaderView.findViewById(R.id.nav_header_image)
+
+        runBlocking {
+            StudentWrapper.getStudentAvatarUrl(studentObject.student_id!!, studentObject.avatar_extension!!) {
+                Glide.with(applicationContext)
+                    .setDefaultRequestOptions(RequestOptions())
+                    .load(it?: R.drawable.ic_baseline_person_24)
+                    .into(navHeaderImage)
+            }
+        }
+
+
+        val navHeaderName: TextView = navHeaderView.findViewById(R.id.nav_header_name)
+        navHeaderName.setText(studentObject.name)
+
 
 
         toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
